584860f1117aaf9f6ba41f476eb88c885634de70
Edo Freriks <edofre@gmail.com>
2016-08-22 20:32:17 +0200

    Missing translations fix and clean URL setting

--- common/config/main.php
+++ common/config/main.php
@@ -1,9 +1,22 @@
 <?php
 return [
-    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
-    'components' => [
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
-    ],
+	'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+	'components' => [
+		'cache'      => [
+			'class' => 'yii\caching\FileCache',
+		],
+		'i18n'       => [
+			'translations' => [
+				'*' => [
+					'basePath'              => '@common/messages',
+					'class'                 => 'yii\i18n\PhpMessageSource',
+					'on missingTranslation' => ['common\components\i18n\TranslationEventHandler', 'handleMissingTranslation'],
+				],
+			],
+		],
+		'urlManager' => [
+			'enablePrettyUrl' => true,
+			'showScriptName'  => false,
+		],
+	],
 ];

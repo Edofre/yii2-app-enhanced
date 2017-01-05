7786b0d0013258fc8a1440b9ffad0526238d24ea
Edo Freriks <edofre@gmail.com>
2017-01-05 10:52:44 +0100

    Formatting config files and testing signing commits

--- api/config/params.php
+++ api/config/params.php
@@ -1,5 +1,5 @@
 <?php
 return [
-	'adminEmail'    => 'admin@example.com',
-	'apiKey'        => ';nbo8h42i3bhfvo8bon937dg73f8ub0-rjadlf',
+    'adminEmail' => 'admin@example.com',
+    'apiKey'     => ';nbo8h42i3bhfvo8bon937dg73f8ub0-rjadlf',
 ];
